77e72d0e5cd0d1434055ae15fe6bac46efb880ab
Jeremiah Valenzuela <github@julianoptics.com>
2019-10-29 16:58:30 -0400

    Change formatting

--- Custom General Functions/uigetfullfile.m
+++ Custom General Functions/uigetfullfile.m
@@ -1,18 +1,18 @@
 function fullfilepath = uigetfullfile(varargin)
-%UIGETFULLFILE
-% output is string array
-
-  [filenames, filepath] = uigetfile(varargin{:});
-  
-  % Check if uigetfile was cancelled out
-  if ~iscell(filenames) && ~ischar(filenames)
-    error('No files selected')
-  end
-  
-  if ischar(filenames)
-    fullfilepath = string([filepath, filenames]);
-  else
-    fullfilepath = string(cellfun(@(x) [filepath, x], filenames, 'UniformOutput', false)');
-  end
-  
+	%UIGETFULLFILE
+	% output is string array
+	
+	[filenames, filepath] = uigetfile(varargin{:});
+	
+	% Check if uigetfile was cancelled out
+	if ~iscell(filenames) && ~ischar(filenames)
+		warning('No files selected')
+		fullfilepath = 0;
+	else
+		if ischar(filenames)
+			fullfilepath = string([filepath, filenames]);
+		else
+			fullfilepath = string(cellfun(@(x) [filepath, x], filenames, 'UniformOutput', false)');
+		end
+	end
 end
