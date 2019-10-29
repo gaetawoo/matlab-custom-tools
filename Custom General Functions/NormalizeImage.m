dc89998b59a383d48e7f16fb56ec9bef33b242d1
Jeremiah Valenzuela <github@julianoptics.com>
2019-10-29 16:56:47 -0400

    Reformat and check if input is more than 1 element

--- Custom General Functions/NormalizeImage.m
+++ Custom General Functions/NormalizeImage.m
@@ -1,11 +1,18 @@
-function normalizedImage = NormalizeImage(imageSingle, newMin, newMax)
-if ~strcmpi(class(imageSingle), 'single') && ~strcmpi(class(imageSingle), 'double')
-  imageSingle = single(imageSingle);
-end
-imageSingle(isinf(imageSingle)) = NaN;
-
-x = (newMax - newMin) / (max(imageSingle(:), [], 'omitnan') - min(imageSingle(:), [], 'omitnan'));
-b = newMax - x * max(imageSingle(:), [], 'omitnan');
-
-normalizedImage = x .* imageSingle + b;
+function normalizedImage = normalizeimage(imageSingle, newMin, newMax)
+	
+	% Array must be size > 1
+	if numel(imageSingle) < 2
+		error('Number of elements in array must be 2 or more.');
+	end
+	
+	% If array is not made of floats, then convert
+  if ~strcmpi(class(imageSingle), 'single') && ~strcmpi(class(imageSingle), 'double')
+    imageSingle = single(imageSingle);
+  end
+  
+  imageSingle(isinf(imageSingle)) = NaN;
+  x = (newMax - newMin) / (max(imageSingle, [], 'all', 'omitnan') - min(imageSingle, [], 'all', 'omitnan'));
+  b = newMax - x * max(imageSingle, [], 'all', 'omitnan');
+  
+  normalizedImage = x .* imageSingle + b;
 end
