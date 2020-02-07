3c889f494a555080bab45e44d5c33c6abbc61a42
Jeremiah Valenzuela <github@julianoptics.com>
2020-02-07 14:41:40 -0500

    Account for greyscale images loaded as RGB

--- Custom General Functions/uiimread.m
+++ Custom General Functions/uiimread.m
@@ -25,13 +25,24 @@ function [out, fullfilepath] = uiimread(title)
   
   hWaitbar = waitbar(0, ['0/', num2str(size(filenamesCell, 1)), ' Images Loaded. Loading: ', strrep(filenamesCell{1}, '_', '\_')]);
   out = imread([filepath, filenamesCell{1}]);
+	if size(out, 3) > 2
+		if isequal(out(:, :, 1), out(:, :, 2), out(:, :, 3))
+			out = out(:, :, 1);
+		end
+	end
   
-  if size(filenamesCell, 1) > 1
-    out = cat(3, out, zeros([size(out), size(filenamesCell, 1) - 1]));
-    for nFiles = 2:size(filenamesCell, 1)
-      waitbar((nFiles - 1) / size(filenamesCell, 1), hWaitbar, [num2str(nFiles - 1), '/', num2str(size(filenamesCell, 1)), ' Images Loaded. Loading: ', strrep(filenamesCell{nFiles}, '_', '\_')]);
-      out(:, :, nFiles) = imread([filepath, filenamesCell{nFiles}]);
-    end
+	if size(filenamesCell, 1) > 1
+		out = cat(3, out, zeros([size(out), size(filenamesCell, 1) - 1]));
+		for nFiles = 2:size(filenamesCell, 1)
+			waitbar((nFiles - 1) / size(filenamesCell, 1), hWaitbar, [num2str(nFiles - 1), '/', num2str(size(filenamesCell, 1)), ' Images Loaded. Loading: ', strrep(filenamesCell{nFiles}, '_', '\_')]);
+			temp = imread([filepath, filenamesCell{nFiles}]);
+			if size(temp, 3) > 2
+				if isequal(temp(:, :, 1), temp(:, :, 2), temp(:, :, 3))
+					temp = temp(:, :, 1);
+				end
+			end
+			out(:, :, nFiles) = temp;
+		end
   end
   waitbar(1, hWaitbar, 'Completed!');
   close(hWaitbar)
