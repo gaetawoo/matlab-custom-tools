ba8419285902a7d2e2f612a6b3cd95d3afb7de32
Jeremiah Valenzuela <github@julianoptics.com>
2020-06-22 16:49:42 -0400

    Add isdeployed flag for compiler

--- startup.m
+++ startup.m
@@ -1,8 +1,10 @@
-try %#ok
-	% https://undocumentedmatlab.com/blog/reverting-axes-controls-in-figure-toolbar
-	if ~verLessThan('matlab','9.5')
-		set(groot,'defaultFigureCreateFcn',@(fig,~)addToolbarExplorationButtons(fig));
+if ~isdeployed
+	try %#ok
+		% https://undocumentedmatlab.com/blog/reverting-axes-controls-in-figure-toolbar
+		if ~verLessThan('matlab','9.5')
+			set(groot,'defaultFigureCreateFcn',@(fig,~)addToolbarExplorationButtons(fig));
+		end
+		setenv('MTFDIR', 'C:\Users\jeremiah.valenzuela\Documents\Projects\Software\18125 Agatha\FFOVMTF');
+		recycle('on')
 	end
-	setenv('MTFDIR', 'C:\Users\jeremiah.valenzuela\Documents\Projects\Software\18125 Agatha\FFOVMTF');
-	recycle('on')
 end
