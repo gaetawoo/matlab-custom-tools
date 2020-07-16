50dc92609e757748316e34949a4ab6f2ba5fe163
Jeremiah Valenzuela <github@julianoptics.com>
2020-07-16 15:24:49 -0400

    feat: Add ability to specify rotation direction

--- Custom General Functions/rotateXYaboutpoint.m
+++ Custom General Functions/rotateXYaboutpoint.m
@@ -1,9 +1,26 @@
-function outputMatrix = rotateXYaboutpoint(xColVector, yColVector, pivotPoint, angleDeg)
+function outputMatrix = rotateXYaboutpoint(xColVector, yColVector, pivotPoint, angleDeg, CWorCCW)
   % From https://www.mathworks.com/matlabcentral/answers/2302-rotation-about-a-point
-  
+	% Positive input angle means CW clockwise rotation
+  if ~exist('CWorCCW', 'var')
+		if angleDeg > 0
+			CWorCCW = 'CW';
+		else
+			CWorCCW = 'CCW';
+		end
+	else
+		if ~strcmpi(CWorCCW, {'CW', 'CCW'})
+			error('CWorCCW input argument must be ''CW'' or ''CCW''')
+		end
+	end
+	
+	if strcmpi(CWorCCW, 'CW')
+		theta = abs(angleDeg);
+	else
+		theta = -abs(angleDeg);
+	end
+	
   D.x = xColVector;
   D.y = yColVector;
-  theta = angleDeg;  % Rotation angle.
   P = pivotPoint;  % Rotation pivot point.
   r = [cosd(theta), -sind(theta); sind(theta), cosd(theta)];
   T = bsxfun(@(x,y) r*x, [D.x.' - P(1); D.y.' - P(2)], false(1,length(D.x)))';
