cba71a3475309140c85614f3add9680dcb65cc15
svn Jeremiah Valenzuela <jeremiah.valenzuela@jenoptik-inc.com>
2020-07-21 20:34:34 +0000

    docs: Add Comments
    git-svn-id: svn://dev-fl-01/JJMatlabCommon/trunk@5 1788185c-8f00-084e-bbed-09a1f43f25a4

--- Custom General Functions/round2multiple.m
+++ Custom General Functions/round2multiple.m
@@ -1,3 +1,27 @@
 function output = round2multiple(data, multiple)
-output = round(data / multiple) * multiple;
+  %ROUND2MULTIPLE  rounds towards nearest integer mupltiple
+  %
+  %   ROUND2MULTIPLE(X, N), for positive integers N, rounds X to nearest multiple
+  %   of N. If N is zero, X does not change.
+  %
+  %   Examples
+  %   --------
+  %   % Round pi to the multiple of 2
+  %   >> round2multiple(pi, 2)
+  %        4
+  %
+  %   % Round 20 to the nearest multiple of 13
+  %   >> round2multiple(20, 13)
+  %        26
+  %
+  %  SEE ALSO FLOOR, CEIL, FPRINTF, ROUND.
+  
+  %   By Jeremiah Valenzuela
+  %   2018
+  
+  if multiple == 0
+    output = data;
+  else
+    output = round(data / multiple) * multiple;
+  end
 end
